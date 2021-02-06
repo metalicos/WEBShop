17f7f4f7335fd6eaff44758a3db9e298be235a7c
Ostap Komplikevych <ostap.komplikevych.ki.2017@lpnu.ua>
2021-02-06 02:34:46 +0200

    commit#2 added Logger, and DB main setup

--- src/main/java/com/ostap/komplikevych/webShop/Main.java
+++
@@ -1,7 +0,0 @@
-package com.ostap.komplikevych.webShop;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Garno");
-    }
-}
---
+++ src/main/java/com/ostap/komplikevych/webshop/constant/Const.java
@@ -0,0 +1,32 @@
+package com.ostap.komplikevych.webshop.constant;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Const {
+    public static final String DB_PROPERTIES = "application.properties";
+    public static final String URL = "db.url";
+    public static final String USER = "db.user";
+    public static final String PASS = "db.password";
+
+    public static final Logger logger = LogManager.getLogger(Const.class.getName());
+
+    public static String getProperty(String key) {
+        Properties props = null;
+        try (FileInputStream fis = new FileInputStream(DB_PROPERTIES)) {
+            props = new Properties();
+            props.load(fis);
+        } catch (IOException e) {
+            Const.logger.error(e.getMessage());
+        }
+        if(props!=null) {
+            return props.getProperty(key);
+        }
+        return null;
+    }
+}
